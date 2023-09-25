9e7e8477a037b1fd42405d9da5158391abef41a8
RyanHu021 <ryanhu021@gmail.com>
2023-09-25 13:35:03 -0700

    organize frontend structure

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Table from "./Table";
-import Form from "./Form";
+import Form from "./components/Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
