cbd32c706b993395771337661217d038462271d1
RyanHu021 <ryanhu021@gmail.com>
2023-09-25 13:39:36 -0700

    add server and users route

---
+++ express-backend/routes/users.js
@@ -0,0 +1,74 @@
+import express from "express";
+
+const router = express.Router();
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+router.get("/", (req, res) => {
+  const { name } = req.query;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+router.post("/", (req, res) => {
+  const userToAdd = req.body;
+  const result = addUser(userToAdd);
+  res.status(201).send(result);
+});
+
+export default router;
---
+++ express-backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import usersRouter from "./routes/users.js";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
